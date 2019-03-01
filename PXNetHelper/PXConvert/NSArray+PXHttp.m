c8452efe4e942fbac3ad71bce4a5421ea3d2b891
= <qipengxiang1990@163.com>
2019-03-01 15:00:04 +0800

    1.6.2

--- PXNetHelper/PXConvert/NSArray+PXHttp.m
+++ PXNetHelper/PXConvert/NSArray+PXHttp.m
@@ -8,81 +8,64 @@
 
 #import "NSArray+PXHttp.h"
 
-#ifdef DEBUG
 @implementation NSArray (PXHttp)
-
+#ifdef DEBUG
 - (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level{
-    NSString *output;
-    @try {
-        output = [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted error:nil] encoding:NSUTF8StringEncoding];
-        output = [output stringByReplacingOccurrencesOfString:@"\\/" withString:@"/"];
-    } @catch (NSException *exception) {
-        output = self.description;
-    } @finally {
-        
+    NSMutableString *desc = [NSMutableString string];
+    NSMutableString *tabString = [[NSMutableString alloc] initWithCapacity:level];
+    for (NSUInteger i = 0; i < level; ++i) {
+        [tabString appendString:@"\t"];
     }
-    return  output;
-}
-
-/*
-NSMutableString *desc = [NSMutableString string];
-
-NSMutableString *tabString = [[NSMutableString alloc] initWithCapacity:level];
-for (NSUInteger i = 0; i < level; ++i) {
-    [tabString appendString:@"\t"];
-}
-
-NSString *tab = @"";
-if (level > 0) {
-    tab = tabString;
-}
-[desc appendString:@"\t(\n"];
-
-for (id obj in self) {
-    if ([obj isKindOfClass:[NSDictionary class]]
-        || [obj isKindOfClass:[NSArray class]]
-        || [obj isKindOfClass:[NSSet class]]) {
-        NSString *str = [((NSDictionary *)obj) descriptionWithLocale:locale indent:level + 1];
-        [desc appendFormat:@"%@\t%@,\n", tab, str];
-    } else if ([obj isKindOfClass:[NSString class]]) {
-        [desc appendFormat:@"%@\t\"%@\",\n", tab, obj];
-    } else if ([obj isKindOfClass:[NSData class]]) {
-        // 如果是NSData类型，尝试去解析结果，以打印出可阅读的数据
-        NSError *error = nil;
-        NSObject *result =  [NSJSONSerialization JSONObjectWithData:obj
-                                                            options:NSJSONReadingMutableContainers
-                                                              error:&error];
-        // 解析成功
-        if (error == nil && result != nil) {
-            if ([result isKindOfClass:[NSDictionary class]]
-                || [result isKindOfClass:[NSArray class]]
-                || [result isKindOfClass:[NSSet class]]) {
-                NSString *str = [((NSDictionary *)result) descriptionWithLocale:locale indent:level + 1];
-                [desc appendFormat:@"%@\t%@,\n", tab, str];
-            } else if ([obj isKindOfClass:[NSString class]]) {
-                [desc appendFormat:@"%@\t\"%@\",\n", tab, result];
-            }
-        } else {
-            @try {
-                NSString *str = [[NSString alloc] initWithData:obj encoding:NSUTF8StringEncoding];
-                if (str != nil) {
-                    [desc appendFormat:@"%@\t\"%@\",\n", tab, str];
-                } else {
-                    [desc appendFormat:@"%@\t%@,\n", tab, obj];
+    NSString *tab = (level > 0) ? tabString : @"";
+    [desc appendString:@"(\n"];
+    for (int i = 0; i < self.count; i++){
+        id obj = self[i];
+        if ([obj isKindOfClass:[NSDictionary class]]
+            || [obj isKindOfClass:[NSArray class]]
+            || [obj isKindOfClass:[NSSet class]]){
+            NSString *str = [((NSDictionary *)obj) descriptionWithLocale:locale indent:level + 1];
+            (i == (self.count - 1)) ? [desc appendFormat:@"%@\t%@\n", tab, str] : [desc appendFormat:@"%@\t%@,\n", tab, str];
+        }else if ([obj isKindOfClass:[NSString class]]){
+            (i == (self.count - 1)) ? [desc appendFormat:@"%@\t\"%@\"\n", tab, obj] : [desc appendFormat:@"%@\t\"%@\",\n", tab, obj];
+        }else if ([obj isKindOfClass:[NSData class]]){
+            // 如果是NSData类型，尝试去解析结果，以打印出可阅读的数据
+            NSError *error = nil;
+            NSObject *result =  [NSJSONSerialization JSONObjectWithData:obj
+                                                                options:NSJSONReadingMutableContainers
+                                                                  error:&error];
+            // 解析成功
+            if (error == nil && result != nil){
+                if ([result isKindOfClass:[NSDictionary class]]
+                    || [result isKindOfClass:[NSArray class]]
+                    || [result isKindOfClass:[NSSet class]]){
+                    NSString *str = [((NSDictionary *)result) descriptionWithLocale:locale indent:level + 1];
+                    (i == (self.count - 1)) ? [desc appendFormat:@"%@\t%@\n", tab, str] : [desc appendFormat:@"%@\t%@,\n", tab, str];
+                    
+                }else if ([obj isKindOfClass:[NSString class]]){
+                    (i == (self.count - 1)) ? [desc appendFormat:@"%@\t\"%@\"\n", tab, result] : [desc appendFormat:@"%@\t\"%@\",\n", tab, result];
+                }
+            }else {
+                @try {
+                    NSString *str = [[NSString alloc] initWithData:obj encoding:NSUTF8StringEncoding];
+                    if (str != nil) {
+                        (i == (self.count - 1)) ? [desc appendFormat:@"%@\t\"%@\"\n", tab, str] : [desc appendFormat:@"%@\t\"%@\",\n", tab, str];
+                    } else {
+                        (i == (self.count - 1)) ? [desc appendFormat:@"%@\t%@\n", tab, obj] : [desc appendFormat:@"%@\t%@,\n", tab, obj];
+                    }
+                }
+                @catch (NSException *exception) {
+                    (i == (self.count - 1)) ? [desc appendFormat:@"%@\t%@\n", tab, obj] : [desc appendFormat:@"%@\t%@,\n", tab, obj];
                 }
             }
-            @catch (NSException *exception) {
-                [desc appendFormat:@"%@\t%@,\n", tab, obj];
-            }
+        } else {
+            (i == (self.count - 1)) ? [desc appendFormat:@"%@\t%@\n", tab, obj] : [desc appendFormat:@"%@\t%@,\n", tab, obj];
         }
-    } else {
-        [desc appendFormat:@"%@\t%@,\n", tab, obj];
     }
+    
+    [desc appendFormat:@"%@)", tab];
+    
+    return desc;
 }
-
-[desc appendFormat:@"%@)", tab];
-
-return desc;
-*/
-@end
 #endif
+@end
+
--- PXNetHelper/PXConvert/NSSet+PXHttp.m
+++ PXNetHelper/PXConvert/NSSet+PXHttp.m
@@ -8,78 +8,84 @@
 
 #import "NSSet+PXHttp.h"
 
-#ifdef DEBUG
 @implementation NSSet (PXHttp)
 
-- (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level{
-    NSString *output;
-    @try {
-        output = [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted error:nil] encoding:NSUTF8StringEncoding];
-        output = [output stringByReplacingOccurrencesOfString:@"\\/" withString:@"/"];
-    } @catch (NSException *exception) {
-        output = self.description;
-    } @finally {
-        
+#if DEBUG
+
+- (NSString *)descriptionWithLocale:(id)locale indent:(NSUInteger)level
+{
+    NSMutableString *desc = [NSMutableString string];
+    
+    NSMutableString *tabString = [[NSMutableString alloc] initWithCapacity:level];
+    for (NSUInteger i = 0; i < level; ++i) {
+        [tabString appendString:@"\t"];
     }
-    return  output;
-}
-/*
-NSMutableString *desc = [NSMutableString string];
-
-NSMutableString *tabString = [[NSMutableString alloc] initWithCapacity:level];
-for (NSUInteger i = 0; i < level; ++i) {
-    [tabString appendString:@"\t"];
-}
-
-NSString *tab = @"\t";
-if (level > 0) {
-    tab = tabString;
-}
-[desc appendString:@"\t{(\n"];
-
-for (id obj in self) {
-    if ([obj isKindOfClass:[NSDictionary class]]
-        || [obj isKindOfClass:[NSArray class]]
-        || [obj isKindOfClass:[NSSet class]]) {
-        NSString *str = [((NSDictionary *)obj) descriptionWithLocale:locale indent:level + 1];
-        [desc appendFormat:@"%@\t%@,\n", tab, str];
-    } else if ([obj isKindOfClass:[NSString class]]) {
-        [desc appendFormat:@"%@\t\"%@\",\n", tab, obj];
-    } else if ([obj isKindOfClass:[NSData class]]) {
-        // 如果是NSData类型，尝试去解析结果，以打印出可阅读的数据
-        NSError *error = nil;
-        NSObject *result =  [NSJSONSerialization JSONObjectWithData:obj
-                                                            options:NSJSONReadingMutableContainers
-                                                              error:&error];
-        // 解析成功
-        if (error == nil && result != nil) {
-            if ([result isKindOfClass:[NSDictionary class]]
-                || [result isKindOfClass:[NSArray class]]
-                || [result isKindOfClass:[NSSet class]]) {
-                NSString *str = [((NSDictionary *)result) descriptionWithLocale:locale indent:level + 1];
-                [desc appendFormat:@"%@\t%@,\n", tab, str];
-            } else if ([obj isKindOfClass:[NSString class]]) {
-                [desc appendFormat:@"%@\t\"%@\",\n", tab, result];
-            }
-        } else {
-            @try {
-                NSString *str = [[NSString alloc] initWithData:obj encoding:NSUTF8StringEncoding];
-                if (str != nil) {
-                    [desc appendFormat:@"%@\t\"%@\",\n", tab, str];
-                } else {
-                    [desc appendFormat:@"%@\t%@,\n", tab, obj];
+    
+    NSString *tab = (level > 0) ? tabString : @"\t";
+    
+    [desc appendString:@"{(\n"];
+    
+    NSArray *tempArray = [self allObjects];
+    
+    for (int i = 0; i < tempArray.count; i++)
+    {
+        id obj = tempArray[i];
+        
+        if ([obj isKindOfClass:[NSDictionary class]]
+            || [obj isKindOfClass:[NSArray class]]
+            || [obj isKindOfClass:[NSSet class]])
+        {
+            NSString *str = [((NSDictionary *)obj) descriptionWithLocale:locale indent:level + 1];
+            (i == (tempArray.count-1)) ? [desc appendFormat:@"%@\t%@\n", tab, str] : [desc appendFormat:@"%@\t%@,\n", tab, str];
+        }
+        else if ([obj isKindOfClass:[NSString class]])
+        {
+            (i == (tempArray.count-1)) ? [desc appendFormat:@"%@\t\"%@\"\n", tab, obj] : [desc appendFormat:@"%@\t\"%@\",\n", tab, obj];
+        }
+        else if ([obj isKindOfClass:[NSData class]])
+        {
+            // 如果是NSData类型，尝试去解析结果，以打印出可阅读的数据
+            NSError *error = nil;
+            NSObject *result =  [NSJSONSerialization JSONObjectWithData:obj
+                                                                options:NSJSONReadingMutableContainers
+                                                                  error:&error];
+            // 解析成功
+            if (error == nil && result != nil)
+            {
+                if ([result isKindOfClass:[NSDictionary class]]
+                    || [result isKindOfClass:[NSArray class]]
+                    || [result isKindOfClass:[NSSet class]])
+                {
+                    NSString *str = [((NSDictionary *)result) descriptionWithLocale:locale indent:level + 1];
+                    (i == (tempArray.count-1)) ? [desc appendFormat:@"%@\t%@\n", tab, str] : [desc appendFormat:@"%@\t%@,\n", tab, str];
+                }
+                else if ([obj isKindOfClass:[NSString class]])
+                {
+                    (i == (tempArray.count-1)) ? [desc appendFormat:@"%@\t\"%@\"\n", tab, result] : [desc appendFormat:@"%@\t\"%@\",\n", tab, result];
                 }
             }
-            @catch (NSException *exception) {
-                [desc appendFormat:@"%@\t%@,\n", tab, obj];
+            else {
+                @try {
+                    NSString *str = [[NSString alloc] initWithData:obj encoding:NSUTF8StringEncoding];
+                    if (str != nil) {
+                        (i == (tempArray.count-1)) ? [desc appendFormat:@"%@\t\"%@\"\n", tab, str] : [desc appendFormat:@"%@\t\"%@\",\n", tab, str];
+                    } else {
+                        (i == (tempArray.count-1)) ? [desc appendFormat:@"%@\t%@\n", tab, obj] : [desc appendFormat:@"%@\t%@,\n", tab, obj];
+                    }
+                }
+                @catch (NSException *exception) {
+                    (i == (tempArray.count-1)) ? [desc appendFormat:@"%@\t%@\n", tab, obj] : [desc appendFormat:@"%@\t%@,\n", tab, obj];
+                }
             }
+        } else {
+            (i == (tempArray.count-1)) ? [desc appendFormat:@"%@\t%@\n", tab, obj] : [desc appendFormat:@"%@\t%@,\n", tab, obj];
         }
-    } else {
-        [desc appendFormat:@"%@\t%@,\n", tab, obj];
     }
+    
+    [desc appendFormat:@"%@)}", tab];
+    
+    return desc;
 }
-[desc appendFormat:@"%@)}", tab];
-return desc;
- */
-@end
+
 #endif
+@end
